57a681742ed2f5ec6d753137802211a6bcf78d95
FanStas <stasfantom10@gmail.com>
2023-02-26 19:44:14 +0100

    added Example004

---
+++ Example004_HelloMasha/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Введите свое имя: ");
+string name = Console.ReadLine();
+if(name.ToLower() == "маша") {                  //<ToLower()> команда, которая делает все символы маленькими
+    Console.WriteLine($"Ура, привет {name}!");
+} else {
+    Console.WriteLine($"Привет, {name}.");
+}

1f1f2a44504b35c72bbdde29ce7f2a7642882282
FanStas <stasfantom10@gmail.com>
2023-02-26 21:17:44 +0100

    added Example005

---
+++ Example005_MaxWight/Program.cs
@@ -0,0 +1,21 @@
+﻿int a = 1;
+int b = 9;
+int c = 3;
+int d = 2;
+int e = 5;
+int max = a;
+
+if (b>max) {
+    max = b;
+}
+if (c>max) {
+    max = c;
+}
+if (d>max) {
+    max = d;
+}
+if (e>max) {
+    max = e;
+}
+
+Console.WriteLine(max);

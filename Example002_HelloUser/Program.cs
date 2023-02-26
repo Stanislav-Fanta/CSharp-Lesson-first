bfcac45e32d30d57fcc89be5b07f377474bf30cc
FanStas <stasfantom10@gmail.com>
2023-02-26 18:40:07 +0100

    tested <Console.Write>

--- Example002_HelloUser/Program.cs
+++ Example002_HelloUser/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите ваше имя: ");
+string name = Console.ReadLine();
+Console.Write("Привет, ");
+Console.Write($"{name}!");

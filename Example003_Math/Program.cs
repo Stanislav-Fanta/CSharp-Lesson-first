11e64313760cc7ecc49f95ad8cfe9ef49a8db5d9
FanStas <stasfantom10@gmail.com>
2023-02-26 19:33:49 +0100

    added Example003_Math and 3 algoritms

---
+++ Example003_Math/Program.cs
@@ -0,0 +1,18 @@
+﻿//Алгоритм сложения чисел//
+// Console.WriteLine("Введите первое и второе число: ");
+// int FirstNumber = int.Parse(Console.ReadLine());
+// int SecondNumber = int.Parse(Console.ReadLine());
+// Console.WriteLine(FirstNumber+SecondNumber);
+
+//Алгоритм деления чисел с десетичными дробями//
+// Console.WriteLine("Введите первое и второе число: ");
+// double FirstNumber = int.Parse(Console.ReadLine());
+// double SecondNumber = int.Parse(Console.ReadLine()); // double это тип данных, потдеживающий дроби и не целые числа
+// Console.WriteLine(FirstNumber/SecondNumber);
+
+//Алгоритм сложения рандомных чисел от 1 до 9
+int FirstNumber = new Random().Next(1, 10); // Команда, которая создает новое рандомное целое число в диапазоне 1 - 9 включительно
+Console.WriteLine(FirstNumber);
+int SecondNumber = new Random().Next(1, 10);
+Console.WriteLine(SecondNumber);
+Console.WriteLine(FirstNumber + SecondNumber);
